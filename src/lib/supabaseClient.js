1c68bb66572a3544d476e6f409a8ba7088b0198b
Shuvrasankha Paul <work.shuvrasankha@gmail.com>
2025-04-29 07:46:27 +0530

    Set up proper environment variable handling

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://yywgadreuosyccwcjmil.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl5d2dhZHJldW9zeWNjd2NqbWlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4MTAyMTMsImV4cCI6MjA2MTM4NjIxM30.mxjv34I6UKBUynasYjYaXdndXwyNb_HpwDNDEoc0PRU"
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase environment variables')
